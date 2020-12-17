8252966eb3cc58e412d31b0f4b9ce92a320a786c
tGodson <tendongze95@gmail.com>
2020-12-17 22:05:54 +0100

    add api key to .env file and add .env fiel to .gitignore

---
+++ src/components/API.js
@@ -0,0 +1,11 @@
+import useSWR from 'swr';
+
+const useUser = (id) => {
+  const { data, error } = useSWR(`/api/user/${id}`, fetcher)
+  return {
+    user: data,
+    isLoading: !error && !data,
+    isError: error
+  }
+}
+export default useUser;
