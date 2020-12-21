21882d572fd92be11b147bcb1ea9878e96926249
tGodson <tendongze95@gmail.com>
2020-12-21 08:47:08 +0100

    removed api constant

--- src/components/API.js
+++ src/components/API.js
@@ -1,7 +1,7 @@
 import useSWR from 'swr';
 
 const fetcher = (...args) => fetch(...args).then(res => res.json())
-const api = process.env.REACT_APP_Github_API_KEY
+//const api = process.env.REACT_APP_Github_API_KEY
 
 const UseUser = () => {
   const { data, error } = useSWR(`https://api.github.com/users`, fetcher)
