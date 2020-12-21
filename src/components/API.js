a31988817ac986f1bca0c00c3dde7f161d3c21c4
tGodson <tendongze95@gmail.com>
2020-12-21 08:41:58 +0100

    removed api from .env file

--- src/components/API.js
+++ src/components/API.js
@@ -4,7 +4,7 @@ const fetcher = (...args) => fetch(...args).then(res => res.json())
 const api = process.env.REACT_APP_Github_API_KEY
 
 const UseUser = () => {
-  const { data, error } = useSWR(`${api}`, fetcher)
+  const { data, error } = useSWR(`https://api.github.com/users`, fetcher)
   return {
     user: data,
     isLoading: !error && !data,
